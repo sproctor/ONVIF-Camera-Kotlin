c1d5b6c8eeeddcc18ad287ee9b9a7522c74d8771
Sean Proctor <sproctor@gmail.com>
2021-02-01 10:59:39 -0500

    restructure package again to fit match maven artifact

--- onvifcamera/src/main/java/com/seanproctor/onvif/onvifcamera/Exceptions.kt
+++
@@ -1,7 +0,0 @@
-package com.seanproctor.onvif.onvifcamera
-
-class OnvifUnauthorized(message: String) : Exception(message)
-
-class OnvifInvalidResponse(message: String) : Exception(message)
-
-class OnvifForbidden(message: String) : Exception(message)
