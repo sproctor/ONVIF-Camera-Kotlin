cee0467b3ad702a6acd8d5041a952de806bd6ad6
Sean Proctor <sproctor@gmail.com>
2021-03-05 10:27:24 -0500

    minor version update with other minor adjustments

--- onvifcamera/src/main/java/com/seanproctor/onvifcamera/OnvifCommands.kt
+++ onvifcamera/src/main/java/com/seanproctor/onvifcamera/OnvifCommands.kt
@@ -4,27 +4,29 @@ object OnvifCommands {
     /**
      * The header for SOAP 1.2 with digest authentication
      */
-    private val soapHeader = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+    private const val soapHeader = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
             "<soap:Envelope " +
             "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
             "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
             "xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\" >" +
             "<soap:Body>"
 
-    private val envelopeEnd = "</soap:Body></soap:Envelope>"
+    private const val envelopeEnd = "</soap:Body></soap:Envelope>"
 
-    val profilesCommand = (
+    const val profilesCommand = (
             soapHeader
                     + "<GetProfiles xmlns=\"http://www.onvif.org/ver10/media/wsdl\"/>"
                     + envelopeEnd
             )
 
-    fun getStreamURICommand(profile: MediaProfile): String {
-
-        return (soapHeader + "<GetStreamUri xmlns=\"http://www.onvif.org/ver20/media/wsdl\">"
-                + "<ProfileToken>" + profile.token + "</ProfileToken>"
-                + "<Protocol>RTSP</Protocol>"
-                + "</GetStreamUri>" + envelopeEnd)
+    fun getStreamURICommand(profile: MediaProfile, protocol: String = "RTSP"): String {
+        return (soapHeader
+                + "<GetStreamUri xmlns=\"http://www.onvif.org/ver20/media/wsdl\">"
+                + "<ProfileToken>${profile.token}</ProfileToken>"
+                + "<Protocol>${protocol}</Protocol>"
+                + "</GetStreamUri>"
+                + envelopeEnd
+                )
     }
 
     fun getSnapshotURICommand(profile: MediaProfile): String {
@@ -34,14 +36,14 @@ object OnvifCommands {
                 + "</GetSnapshotUri>" + envelopeEnd)
     }
 
-    val deviceInformationCommand = (
+    const val deviceInformationCommand = (
             soapHeader
                     + "<GetDeviceInformation xmlns=\"http://www.onvif.org/ver10/device/wsdl\">"
                     + "</GetDeviceInformation>"
                     + envelopeEnd
             )
 
-    val servicesCommand = (
+    const val servicesCommand = (
             soapHeader
                     + "<GetServices xmlns=\"http://www.onvif.org/ver10/device/wsdl\">"
                     + "<IncludeCapability>false</IncludeCapability>"
