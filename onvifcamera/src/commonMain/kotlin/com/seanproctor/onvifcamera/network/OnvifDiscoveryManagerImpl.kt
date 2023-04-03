57f90ea00dfd6e12b69e624608ae03fe2c0425f7
Sean Proctor <sproctor@gmail.com>
2023-04-03 16:45:43 -0400

    use persistent maps to store devices

--- onvifcamera/src/commonMain/kotlin/com/seanproctor/onvifcamera/network/OnvifDiscoveryManagerImpl.kt
+++ onvifcamera/src/commonMain/kotlin/com/seanproctor/onvifcamera/network/OnvifDiscoveryManagerImpl.kt
@@ -2,12 +2,15 @@ package com.seanproctor.onvifcamera.network
 
 import com.seanproctor.onvifcamera.DiscoveredOnvifDevice
 import com.seanproctor.onvifcamera.parseOnvifProbeResponse
+import kotlinx.collections.immutable.mutate
+import kotlinx.collections.immutable.persistentHashMapOf
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import java.net.InetAddress
 
 internal class OnvifDiscoveryManagerImpl(
@@ -16,36 +19,37 @@ internal class OnvifDiscoveryManagerImpl(
     override fun discoverDevices(retryCount: Int, scope: CoroutineScope): Flow<List<DiscoveredOnvifDevice>> {
         require(retryCount > 0) { "Retry count must be greater than 0" }
 
-        val discoveredDevices = mutableMapOf<InetAddress, DiscoveredOnvifDevice>()
-        val mutex = Mutex()
-        val packetFlow = socketListener.listenForPackets(retryCount)
-        return packetFlow.map { packet ->
-            if (!discoveredDevices.containsKey(packet.address)) {
-                scope.launch {
-                    val data = packet.data.decodeToString()
-                    try {
-                        val result = parseOnvifProbeResponse(data)
-                        if (result.size == 1) {
-                            val probeMatch = result.first()
-                            val device = DiscoveredOnvifDevice(
-                                id = probeMatch.endpointReference.address,
-                                types = probeMatch.types?.split(" ") ?: emptyList(),
-                                scopes = probeMatch.scopes?.split(" ") ?: emptyList(),
-                                addresses = probeMatch.xaddrs?.split(" ")
-                                    ?: emptyList(),
-                            )
-                            mutex.withLock {
-                                discoveredDevices.put(packet.address, device)
+        val discoveredDevices = MutableStateFlow(persistentHashMapOf<InetAddress, DiscoveredOnvifDevice>())
+        val job = scope.launch {
+            socketListener.listenForPackets(retryCount)
+                .collect { packet ->
+                    launch {
+                        val data = packet.data.decodeToString()
+                        try {
+                            val result = parseOnvifProbeResponse(data)
+                            if (result.size == 1) {
+                                val probeMatch = result.first()
+                                val device = DiscoveredOnvifDevice(
+                                    id = probeMatch.endpointReference.address,
+                                    types = probeMatch.types?.split(" ") ?: emptyList(),
+                                    scopes = probeMatch.scopes?.split(" ") ?: emptyList(),
+                                    addresses = probeMatch.xaddrs?.split(" ")
+                                        ?: emptyList(),
+                                )
+                                discoveredDevices.update { deviceMap ->
+                                    deviceMap.mutate {
+                                        it[packet.address] = device
+                                    }
+                                }
                             }
+                        } catch (e: Throwable) {
+                            e.printStackTrace()
                         }
-                    } catch (e: Throwable) {
-                        println(packet.address)
-                        println(data)
-                        e.printStackTrace()
                     }
                 }
-            }
-            discoveredDevices.values.toList()
         }
+        return discoveredDevices
+            .map { it.values.toList() }
+            .onCompletion { job.cancel() }
     }
 }
