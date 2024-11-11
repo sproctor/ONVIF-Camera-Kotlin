7862b59fb7d4d9dfaabda1d89853eeff311c4a0c
Sean Proctor <sproctor@gmail.com>
2024-11-11 09:54:24 -0500

    simplify logger signature

--- onvifcamera/src/commonMain/kotlin/com/seanproctor/onvifcamera/OnvifLogger.kt
+++ onvifcamera/src/commonMain/kotlin/com/seanproctor/onvifcamera/OnvifLogger.kt
@@ -1,7 +1,6 @@
 package com.seanproctor.onvifcamera
 
 public interface OnvifLogger {
-    public fun error(message: String)
-    public fun error(message: String, e: Throwable)
+    public fun error(message: String, e: Throwable?)
     public fun debug(message: String)
 }
